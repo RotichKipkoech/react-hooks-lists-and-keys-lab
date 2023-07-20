a7c886d2a2a85f0672121d3b0929195f1015ceb1
RotichKipkoech <kipkoechrottich@gmail.com>
2023-07-20 14:56:13 +0300

    Complitted Assignment

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,39 @@
+// import React from "react";
+// import ProjectItem from "./ProjectItem";
+
+// function ProjectList({ projects }) {
+//   console.log(projects);
+//   return (
+//     <div id="projects">
+//       <h2>My Projects</h2>
+//       <div id="project-list">{/* render ProjectItem components here */}</div>
+//     </div>
+//   );
+// }
+
+// export default ProjectList;
+
+
+
+
+
 import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projects.map((project) => (
+          <ProjectItem
+            key={project.id} // Assuming each project has an 'id' property
+            name={project.name}
+            about={project.about}
+            technologies={project.technologies}
+          />
+        ))}
+      </div>
     </div>
   );
 }
